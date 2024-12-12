724ea18e2a8d589062da3147faebbac7376df87e
Lukas Moro <lukas.moro@me.com>
2024-12-12 09:06:23 +0100

    refactoring: ForEach CardView

--- CS193p_Memorize/CS193p_Memorize/ContentView.swift
+++ CS193p_Memorize/CS193p_Memorize/ContentView.swift
@@ -8,16 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let emojis = ["🦋", "🪲", "🐝"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack  {
+            ForEach(emojis.indices, id: \.self){ index in
+                CardView(content: emojis[index])
+            }
         }
         .padding()
     }
 }
+
+struct CardView: View {
+   
+    @State var isFaceUp = true
+    var content = "🦋"
+    
+    var body: some View {
+        ZStack  {
+            let base = RoundedRectangle(cornerRadius: 30)
+            
+            if isFaceUp{
+                base.foregroundColor(Color.gray)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }
+            else{
+                base.foregroundColor(Color.blue)
+            }
+        }
+        .onTapGesture {
+            print("tapped")
+            isFaceUp .toggle()
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
